046d37dda725770fe4a197edb96def3777b1362f
pavell00 <mymmo88@gmail.com>
2017-03-14 15:43:04 +0300

    ver1_0

--- app/services/app.service.ts
+++ app/services/app.service.ts
@@ -62,43 +62,43 @@ export class AppService {
   getBCramber(){return this.bcramberSource;}
 
   saveDocPromise(d: Document){
-      let headers = new Headers({ 'Content-Type': 'application/json' });
-      let options = new RequestOptions({ headers: headers });
-      this.http.post(this.docmentsUrl, JSON.stringify(d), options)
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    this.http.post(this.docmentsUrl, JSON.stringify(d), options)
         .toPromise()
         .then(response => response.json().data)
         .catch(this.handleError)
-      this.searchDocs4();
+    this.searchDocs4();
   }
 
   updateDocPromise(d: Document){
-      let headers = new Headers({ 'Content-Type': 'application/json' });
-      let options = new RequestOptions({ headers: headers });
-      this.http.put(`app/documents/${d['id']}`, JSON.stringify(d), options)
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    this.http.put(`app/documents/${d['id']}`, JSON.stringify(d), options)
         .toPromise()
         .then(response => response.json())
         .catch(this.handleError)
   }
 
   deleteDocPromise(id: string){
-      /*this.docs.forEach(element => {
-          console.log(element)
-      });*/
-      let headers = new Headers({ 'Content-Type': 'application/json' });
-      let options = new RequestOptions({ headers: headers });
-      console.log(`app/documents/${id}`);
-      this.http.delete(`app/documents/${id}`, options)
+    /*this.docs.forEach(element => {
+         console.log(element)
+    });*/
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    console.log(`app/documents/${id}`);
+    this.http.delete(`app/documents/${id}`, options)
         .toPromise()
         .then(response => response.json())
         .catch(this.handleError)
-      this.searchDocs4();
+     this.searchDocs4();
   }
 
   searchFolder () {
-      //console.log(this.currentFolder);
-      let params = new URLSearchParams();
-      params.set('rootId', this.currentFolderId);
-      let a = this.http
+    //console.log(this.currentFolder);
+    let params = new URLSearchParams();
+    params.set('rootId', this.currentFolderId);
+    let a = this.http
         .get(this.foldersUrl, { search: params })
         .map(response => <Folder[]> response.json().data)
             a.subscribe(
@@ -106,17 +106,16 @@ export class AppService {
                 },
                 (err) => (this.handleError)
             )
-      //return a;
+    //return a;
   }
 
   searchDocs4 () {
-      let term = this.currentFolderId;
-      let currentDate = this.calendar.getValue();//this.calendar;
-      let params = new URLSearchParams();
-      params.set('fldId', term);
-      params.set('dateItem', currentDate);
-
-      let a = this.http
+     let term = this.currentFolderId;
+     let currentDate = this.calendar.getValue();//this.calendar;
+     let params = new URLSearchParams();
+     params.set('fldId', term);
+     params.set('dateItem', currentDate);
+     let a = this.http
         .get(this.docmentsUrl, { search: params })
         .map(response => <Document[]> response.json().data)
             a.subscribe(
@@ -124,26 +123,26 @@ export class AppService {
                 },
                 (err) => (this.handleError)
             )
-      return a;
+     return a;
   }
 
   searchJournal (term: string) {
-      let params = new URLSearchParams();
-      params.set('docId', term);
-      let a = this.http
+    let params = new URLSearchParams();
+    params.set('docId', term);
+    let a = this.http
         .get(this.journalsUrl, { search: params })
         .map(response => <Journal[]> response.json().data)
             a.subscribe(
                 (val) => {this.journals.next(val);},//without filtering
                 (err) => (this.handleError)
             )
-      return a;
+    return a;
   }
 
   searchEntity(term: string) {
-      let params = new URLSearchParams();
-      params.set('name', term);
-      let a = this.http
+    let params = new URLSearchParams();
+    params.set('name', term);
+    let a = this.http
         .get(this.entitiesUrl, { search: params })
         .map(response => <Entity[]> response.json().data)
             a.subscribe(
@@ -154,15 +153,15 @@ export class AppService {
                 },
                 (err) => (this.handleError)
             )
-      return a;
+    return a;
   }
 
   private extractData(res: Response) {
-      let body = res.json();
-      //console.log(body);
-      //this.todo_2.next(body.data);
-      return body.data || { };
-      //return body.data || { };
+    let body = res.json();
+    //console.log(body);
+    //this.todo_2.next(body.data);
+    return body.data || { };
+    //return body.data || { };
   }
 
   private handleError(error: any) {
@@ -181,29 +180,28 @@ export class AppService {
   }
 
   saveFolderPromise(f: Folder){
-      let headers = new Headers({ 'Content-Type': 'application/json' });
-      let options = new RequestOptions({ headers: headers });
-      this.http.post(this.foldersUrl, JSON.stringify(f), options)
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    this.http.post(this.foldersUrl, JSON.stringify(f), options)
         .toPromise()
         .then(response => response.json().data)
         .catch(this.handleError)
   }
 
   saveDocObs(d: Document): Observable<Document>{
-      //console.log(JSON.stringify(d));
-      let headers = new Headers({ 'Content-Type': 'application/json' });
-      let options = new RequestOptions({ headers: headers });
-      return this.http.post(this.docmentsUrl, JSON.stringify(d), options)
-            .map(this.extractData)
-            .catch(this.handleError);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(this.docmentsUrl, JSON.stringify(d), options)
+        .map(this.extractData)
+        .catch(this.handleError);
     }
 
   // ??
   searchDocs2(term : string): Observable<Document[]>  {
-      return this.http
-               .get(`app/documents/?fldId=${1}`)
-               .map(response => response.json().data)
-               .catch(this.handleError);
+    return this.http
+        .get(`app/documents/?fldId=${1}`)
+        .map(response => response.json().data)
+        .catch(this.handleError);
       //this.http
       //  .get(`app/documents.json/?fldId=${1}`)
       //  .map((r: Response) => r.json().data as Document[])
@@ -250,22 +248,22 @@ export class AppService {
       let params = new URLSearchParams();
       params.set('fldId', term);
       let a = this.http
-               .get(this.docmentsUrl, { search: params })
-               .map(response => response.json().data)
+            .get(this.docmentsUrl, { search: params })
+            .map(response => response.json().data)
       return a;
   }
 
   search2() : Promise<Document[]> {
       return this.http.get(this.docmentsUrl)
-               .toPromise()
-               .then(response => response.json().data)
-               .catch(this.handleError);
+            .toPromise()
+            .then(response => response.json().data)
+            .catch(this.handleError);
   }
 
   search3() : Promise<Journal[]> {
       return this.http.get(this.journalsUrl)
-               .toPromise()
-               .then(response => response.json().data)
-               .catch(this.handleError);
+            .toPromise()
+            .then(response => response.json().data)
+            .catch(this.handleError);
   }
 }
